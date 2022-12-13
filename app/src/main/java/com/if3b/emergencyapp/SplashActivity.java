ff3b2ec1af498029149c36aa472115c88ace7546
bebinPaula <azzar.arza18@gmail.com>
2022-12-13 23:52:34 +0700

    nambahin splash screen

---
+++ app/src/main/java/com/if3b/emergencyapp/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.if3b.emergencyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Handler HDL = new Handler();
+        HDL.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        },2000);
+    }
+
+}
