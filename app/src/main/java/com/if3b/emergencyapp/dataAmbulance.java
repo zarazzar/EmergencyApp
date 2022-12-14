34ae903e422cef88ac02feb53d9d0b7f44ad2ccd
zarazzar <azzar.arza18@gmail.com>
2022-12-14 15:17:39 +0700

    masukan data.java

---
+++ app/src/main/java/com/if3b/emergencyapp/dataAmbulance.java
@@ -0,0 +1,13 @@
+package com.if3b.emergencyapp;
+
+public class dataAmbulance {
+    public static String[][] data = new String[][]{
+            //nama  ,alamat, no telp, urlLokasi, , foto.
+            {"UGD RS Bhayangkara Palembang", "Jl. Jend Sudirman No.Km. 4, RW.5, Ario Kemining, Kec.Kemuning, Palembang, Sumatera Selatan", "0711410023","https://maps.app.goo.gl/iZYjNgRCFomGgk6N7 ",""},
+            {"IGD RSU Bunda Palembang", "Jl. Demang Lebar Daun Ilir Barat I, Palembang Sumatera Selatan", "119", "https://maps.app.goo.gl/rRFTBMbr8MRoEYdB8",""},
+
+
+//
+
+    };
+}
