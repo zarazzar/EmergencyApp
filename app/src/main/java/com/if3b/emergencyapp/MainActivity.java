3146027a742e85eb4e55a52e380b2ed86673adf9
zarazzar <azzar.arza18@gmail.com>
2022-12-14 14:14:42 +0700

    update Main Activity

--- app/src/main/java/com/if3b/emergencyapp/MainActivity.java
+++ app/src/main/java/com/if3b/emergencyapp/MainActivity.java
@@ -3,12 +3,24 @@ package com.if3b.emergencyapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView ivDamkar,ivAmbulan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ivAmbulan = findViewById(R.id.iv_ambulan);
+
+        ivAmbulan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
