1c9ad6761aa1f96239f80365d428cd48cac1bdd0
zarazzar <azzar.arza18@gmail.com>
2022-12-15 23:26:58 +0700

    beneri detail activity

---
+++ app/src/main/java/com/if3b/emergencyapp/DetailActivityDamkar.java
@@ -0,0 +1,14 @@
+package com.if3b.emergencyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailActivityDamkar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_damkar);
+    }
+}
