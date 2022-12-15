1d94430a028530735b59114526587bcca420dff7
zarazzar <azzar.arza18@gmail.com>
2022-12-15 22:23:24 +0700

    input data Damkar

--- app/src/main/java/com/if3b/emergencyapp/dataDamkar.java
+++ app/src/main/java/com/if3b/emergencyapp/dataDamkar.java
@@ -3,6 +3,17 @@ package com.if3b.emergencyapp;
 public class dataDamkar {
     public static String[][] data = new String[][]{
             //nama  ,alamat, no telp, urlLokasi, , foto.
+            {"DAMKAR Jakarta Fire And Disaster Management Palembang", "Jl. Merdeka No.8, 19 Ilir, Kec. Bukit Kecil, Kota Palembang, Sumatera Selatan 30113", "0711312011","https://maps.app.goo.gl/eJAZbGsg2f3SdJpWA","https://drive.google.com/file/d/1Vz7ItWVIRwyUroUsgj2qn1eFOXISUQaY/view?usp=drivesdk"},
+
+            {"DAMKAR Talang Keramat Kenten Palembang", "Jl. Talang Keramat, Kebun Bunga, Kec. Sukarami, Kota Palembang, Sumatera Selatan 30961", "07115712333","https://maps.app.goo.gl/mN9XL7yo4uGwiHJu5","https://drive.google.com/file/d/1VzD_U9Brbcwp95PCDlCkCAfBDJj0oAiZ/view?usp=drivesdk"},
+
+            {"DAMKAR Sako Sematang Borang Palembang", "Kec. Sako, Kota Palembang, Sumatera Selatan 30961", "0711822532","https://maps.app.goo.gl/gJBehUjZr2qoGdjb6","https://drive.google.com/file/d/1Vzxli0eFTsm1YnJbLxmPLRqdPWRQF5rO/view?usp=drivesdk"},
+
+            {"DAMKAR Kemuning", "Jl. R. Sukamto Jl. Rawasari, 20 Ilir D II, Kec. Ilir Tim. II, Kota Palembang, Sumatera Selatan 30114", "07115556246","https://maps.app.goo.gl/XdL9qT9sPAidAJQh9","https://drive.google.com/file/d/1W14ivA0AQTWOQtCy87hcBoEU7b1h8pzP/view?usp=drivesdk"},
+
+            {"DAMKAR Sebrang Ulu 2, 13 Ulu", "Jl. KH. Azhari, 13 Ulu, Kec. Seberang Ulu II, Kota Palembang, Sumatera Selatan 30263", "07115745387","https://maps.app.goo.gl/5qP8UxnQEfVDG8BA7","https://drive.google.com/file/d/1W17kEma-4sy8862DQnRrpSiPmaeUNhEG/view?usp=drivesdk"},
+
+            {"DAMKAR Alang-Alang Lebar", "Jl. Karya Baru, Kec. Alang-Alang Lebar, Kota Palembang, Sumatera Selatan 30961", "07117423500","https://maps.app.goo.gl/5qP8UxnQEfVDG8BA7","https://drive.google.com/file/d/1W1HQ2rKnsZjKz-tT7NN6adFK2KzZlPmU/view?usp=drivesdk"},
 
 
 
