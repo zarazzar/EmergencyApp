5de1bf1bb51f43be91fad1b060c3df53209d7b16
oriell754 <fernand0753@mhs.mdp.ac.id>
2022-12-19 10:57:51 +0700

    update

---
+++ app/src/main/java/com/if3b/emergencyapp/AdaptercardUGD.java
@@ -0,0 +1,58 @@
+package com.if3b.emergencyapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class AdaptercardUGD  extends  RecyclerView.Adapter<AdaptercardUGD.classviewholder>{
+private ArrayList<ModelUGD> modelUGD;
+
+
+    public AdaptercardUGD(ArrayList<ModelUGD> modelUGD) {
+        this.modelUGD = modelUGD;
+    }
+
+    @NonNull
+    @Override
+    public classviewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_card_ugd,parent,false);
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull classviewholder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return modelUGD.size();
+    }
+
+    public  class classviewholder extends RecyclerView.ViewHolder{
+        ImageView ivfotoUGD;
+        TextView tvnamaUGD,tvalamatUGD;
+        Button btnhubungiUGD,btnlokasiUGD;
+
+        public classviewholder(@NonNull View itemView) {
+            super(itemView);
+            ivfotoUGD= itemView.findViewById(R.id.iv_fotoUGD);
+            tvalamatUGD= itemView.findViewById(R.id.tv_alamatUGD);
+            tvnamaUGD= itemView.findViewById(R.id.tv_namaUGD);
+            btnhubungiUGD= itemView.findViewById(R.id.btn_hubungiUGD);
+            btnlokasiUGD=itemView.findViewById(R.id.btn_lokasiUGD);
+        }
+    }
+
+
+}
+
