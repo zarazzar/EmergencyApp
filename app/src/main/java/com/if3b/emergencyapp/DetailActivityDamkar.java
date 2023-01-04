e49dfc5fdb4abe1ceea276a2f45451a1e4bf3547
zarazzar <azzar.arza18@gmail.com>
2023-01-04 08:29:00 +0700

    updet adapter

--- app/src/main/java/com/if3b/emergencyapp/DetailActivityDamkar.java
+++ app/src/main/java/com/if3b/emergencyapp/DetailActivityDamkar.java
@@ -2,13 +2,54 @@ package com.if3b.emergencyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 public class DetailActivityDamkar extends AppCompatActivity {
+
+//    private ImageView ivFotoDamkar;
+//    private TextView tvNamaDamkar, tvAlamatDamkar;
+//    private Button btnLokasiDamkar, btnHPDamkar;
+//    private String yNamaDamkar,yAlamatDamkar,yFotoDamkar,yLokasiDamkar,yNomorDamkar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_damkar);
+
+//        ivFotoDamkar = findViewById(R.id.iv_foto_damkar);
+//        tvNamaDamkar = findViewById(R.id.tv_nama_damkar);
+//        tvAlamatDamkar = findViewById(R.id.tv_alamat_damkar);
+//        btnLokasiDamkar = findViewById(R.id.btn_lokasi_damkar);
+//        btnHPDamkar = findViewById(R.id.btn_hubungi_damkar);
+//
+//        Intent GetD = getIntent(); //getD tu getDamkar
+//        yNamaDamkar = GetD.getStringExtra("xFotoDamkar");
+//        yAlamatDamkar = GetD.getStringExtra("xAlamatDamkar");
+//        yFotoDamkar = GetD.getStringExtra("xFotoDamkar");
+//        yNomorDamkar = GetD.getStringExtra("xNomorDamkar");
+//        yLokasiDamkar = GetD.getStringExtra("xLokasiDamkar");
+
+
+//        Glide
+//                .with(DetailActivityDamkar.this)
+//                .load(yFotoDamkar)
+//                .into(ivFotoDamkar);
+//        tvNamaDamkar.setText(yNamaDamkar);
+//        tvAlamatDamkar.setText(yAlamatDamkar);
+//
+//        btnLokasiDamkar.setText(yLokasiDamkar);
+//        btnHPDamkar.setText(yNomorDamkar);
+
+
+
+
+
+
     }
 }
