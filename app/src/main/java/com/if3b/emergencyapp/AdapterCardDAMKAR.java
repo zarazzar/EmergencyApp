ce11f643cedb0046a94dd8652fc700cb97832bdd
oriell754 <fernand0753@mhs.mdp.ac.id>
2023-01-04 15:00:23 +0700

    adapter Damkar

--- app/src/main/java/com/if3b/emergencyapp/AdapterCardDAMKAR.java
+++ app/src/main/java/com/if3b/emergencyapp/AdapterCardDAMKAR.java
@@ -1,5 +1,5 @@
 package com.if3b.emergencyapp;
-
+import android.net.Uri;
 import android.content.Context;
 import android.content.Intent;
 import android.view.LayoutInflater;
@@ -57,14 +57,30 @@ public class AdapterCardDAMKAR extends RecyclerView.Adapter<AdapterCardDAMKAR.Da
     {
         ImageView ivFotoDamkar;
         TextView tvNamaDamkar,tvAlamatDamkar;
-        //Button btnHubungi, btnLokasi;
+        Button btnHubungi, btnLokasi;
+        String getlokasiDamkar;
+
         public DamkarViewHolder(@NonNull View itemView) {
             super(itemView);
             ivFotoDamkar = itemView.findViewById(R.id.iv_foto_damkar);
             tvNamaDamkar = itemView.findViewById(R.id.tv_nama_damkar);
             tvAlamatDamkar = itemView.findViewById(R.id.tv_alamat_damkar);
-            //btnHubungi = itemView.findViewById(R.id.btn_hubungi_damkar);
-            //btnLokasi = itemView.findViewById(R.id.btn_hubungi_damkar);
+            btnHubungi = itemView.findViewById(R.id.btn_hubungi_damkar);
+            btnLokasi = itemView.findViewById(R.id.btn_hubungi_damkar);
+
+            btnLokasi.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                   getlokasiDamkar=tvAlamatDamkar.getText().toString();
+
+                    Uri location = Uri.parse("geo:0,0?q=" + getlokasiDamkar);
+                    Intent bukaLokasi = new Intent(Intent.ACTION_VIEW, location);
+                    ctx.startActivity(bukaLokasi);
+                }
+
+
+
+            });
         }
     }
 }
