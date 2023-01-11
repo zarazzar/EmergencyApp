00e721bcb6a92d9eda4ab20b4e639e7a150d909c
zarazzar <azzar.arza18@gmail.com>
2023-01-12 01:55:39 +0700

    buat halaman about

---
+++ app/src/main/java/com/if3b/emergencyapp/AboutUs.java
@@ -0,0 +1,14 @@
+package com.if3b.emergencyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AboutUs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+    }
+}
